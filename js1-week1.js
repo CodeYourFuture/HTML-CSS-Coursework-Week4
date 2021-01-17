7ff1ec3c3471ff9f4c67046d275cf22b51613c40
ASLANDENIZ <36959348+ASLANDENIZ@users.noreply.github.com>
2021-01-17 11:38:35 +0000

    new

---
+++ js1-week1.js
@@ -0,0 +1,10 @@
+console.log("Hello World!")
+
+console.log("My name is Aslan")
+console.log("I love coding")
+console.log("Halo, dunia! // Indonesian") 
+console.log("Ciao, mondo! // Italian")
+console.log("Hola, mundo! // Spanish")
+
+let greeting = "Hello World"
+console.log(greeting)
