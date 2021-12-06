a2c7add75ca8b66119a7f7984edd1ea23f99b68f
Matildaako <matildaako@gmail.com>
2021-12-06 14:15:04 +0000

    Text in the header now changes periodically

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
+// CHANGING TEXT
+var text = ["We Deliver to Your Doorstep", "Call Us Now! 123-456-7890"];
+var counter = 0;
+var elem = document.getElementById("changing-text");
+var inst = setInterval(changeText, 5000);
+
+function changeText() {
+    elem.innerHTML = text[counter];
+    counter++;
+    if (counter >= text.length) {
+        counter = 0;
+    }
+}
+
+changeText();
+
 // MAP
 function initMap() {
     const prickles = { lat: 37.77115113296127, lng: -122.38714525546828 };
