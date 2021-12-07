4e1e7521adcef7523d1697a8f6a6fc387db2c98c
Matildaako <matildaako@gmail.com>
2021-12-07 21:41:25 +0000

    Fixed problem with the menu toggle

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // BRING UP MENU
 function showMenu() {
     var x = document.getElementById("menu-list");
-    if (x.style.display === "none") {
+    if (x.style.display === "none" || x.style.display === "") {
         x.style.display = "flex";
         document.getElementById("bar-two").style.opacity = "0";
         document.getElementById("body").style.overflow = "hidden";
@@ -49,14 +49,14 @@ function changeText() {
 changeText();
 
 // MAP
-function initMap() {
-    const prickles = { lat: 37.77115113296127, lng: -122.38714525546828 };
-    const map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 15,
-        center: prickles,
-    });
-    const marker = new google.maps.Marker({
-        position: prickles,
-        map: map,
-    });
-}
+// function initMap() {
+//     const prickles = { lat: 37.77115113296127, lng: -122.38714525546828 };
+//     const map = new google.maps.Map(document.getElementById("map"), {
+//         zoom: 15,
+//         center: prickles,
+//     });
+//     const marker = new google.maps.Marker({
+//         position: prickles,
+//         map: map,
+//     });
+// }
