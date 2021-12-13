71fef65d6fd3e10077012df87f7546d9f4be34f4
mahri09 <63245237+mahri09@users.noreply.github.com>
2021-12-13 23:35:50 +0000

    added side bar on the home page of desktop version

--- script.js
+++ script.js
@@ -1,17 +1,30 @@
 let toggle = false;
 
-var hamburger = document.getElementById("hamburger-menu");
+var hamburgerMenu = document.getElementById("hamburger-menu");
 var navbar = document.getElementById("navbar");
+let hamburgerTop = document.getElementById("top");
+let hamburgerMiddle = document.getElementById("middle");
 
-hamburger.addEventListener("click", function () {
+hamburgerMenu.addEventListener("click", function () {
   // console.log("hello");
   toggle = !toggle;
   console.log(toggle);
   if (toggle) {
-    navbar.classList.add("navbar");
     navbar.classList.remove("hide");
+    if (window.screen.width > 900) {
+      navbar.classList.add("sidebar");
+      hamburgerTop.style.borderColor = "#fff";
+      hamburgerMiddle.style.borderColor = "#fff";
+      hamburgerMenu.style.gridColumn = "4/5";
+    } else {
+      navbar.classList.add("navbar");
+    }
   } else {
     navbar.classList.remove("navbar");
+    navbar.classList.remove("sidebar");
+    hamburgerTop.style.borderColor = "#000";
+    hamburgerMiddle.style.borderColor = "#000";
     navbar.classList.add("hide");
+    hamburgerMenu.style.gridColumn = "1/2";
   }
 });
