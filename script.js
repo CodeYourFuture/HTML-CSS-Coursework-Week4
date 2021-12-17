9f5fb3f2b382cebce835c8b281a6bbe2c7f255af
sobiawaqar <91378734+sobiawaqar@users.noreply.github.com>
2021-12-17 18:46:04 +0000

    project submitted

---
+++ script.js
@@ -0,0 +1,8 @@
+
+  
+function onClickMenu(){
+	document.getElementById("menu").classList.toggle("change");
+	document.getElementById("nav").classList.toggle("change");
+	
+	document.getElementById("menu-bg").classList.toggle("change-bg");
+}
