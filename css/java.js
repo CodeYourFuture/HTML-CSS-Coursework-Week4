73b3d5cb7c28d757ab53325243ae2d89fbde691a
Kabelo Letlala <86678850+M3ntalist@users.noreply.github.com>
2022-06-21 19:41:47 +0200

    Added more html and css

---
+++ css/java.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    var x = document.getElementById("links");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
