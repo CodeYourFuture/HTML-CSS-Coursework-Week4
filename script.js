6ce7bea955865b6b93b086a0404ccd773d48ad36
Alex-Phillip <105069932+Alex-Phillip@users.noreply.github.com>
2022-07-28 18:10:56 +0100

    added sticky header

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Hamburger Menu
+
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 
@@ -15,4 +17,21 @@ navLink.forEach(n => n.addEventListener("click", closeMenu));
 function closeMenu() {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
+}
+
+
+// Sticky Header
+
+window.onscroll = function() {myFunction()};
+
+var header = document.getElementById("myHeader");
+
+var sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
 }
