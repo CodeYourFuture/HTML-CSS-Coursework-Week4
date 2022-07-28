63078ff3b79348515dd1e19097f7375b9c16b34f
irina mihai <irinamihai150@gmail.com>
2022-07-28 22:07:21 +0100

    indenting code

--- script.js
+++
@@ -1,8 +0,0 @@
-function myFunction() {
-  var x = document.getElementById("myLinks");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
-}
