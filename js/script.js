9d6ca23d4d2d8e4a18853378fec1853c638d0d0f
simeonbikov <bikovsem@gmail.com>
2022-07-29 22:37:45 +0100

    added comments & sorted code

--- js/script.js
+++ js/script.js
@@ -1,18 +1,29 @@
 // document.div.classList.add(".need-animate");
 
-var velocity = 0.5;
 
-function update() {
-  var pos = $(window).scrollTop();
-  $(".container").each(function () {
-    var $element = $(this);
-    // subtract some from the height b/c of the padding
-    var height = $element.height() - 18;
-    $(this).css(
-      "backgroundPosition",
-      "50% " + Math.round((height - pos) * velocity) + "px"
-    );
-  });
-}
+// var velocity = 0.5;
 
-$(window).bind("scroll", update);
+// function update() {
+//   var pos = $(window).scrollTop();
+//   $(".container").each(function () {
+//     var $element = $(this);
+//     // subtract some from the height b/c of the padding
+//     var height = $element.height() - 18;
+//     $(this).css(
+//       "backgroundPosition",
+//       "50% " + Math.round((height - pos) * velocity) + "px"
+//     );
+//   });
+// }
+
+// $(window).bind("scroll", update);
+
+
+// function parallax() {
+//   var scrolled = $(window).scrollTop();
+//   $(".section-one").css({ transform: "translate(0px," + scrolled / 2 + "%" });
+// }
+
+// $(window).scroll(function (e) {
+//   parallax();
+// });
