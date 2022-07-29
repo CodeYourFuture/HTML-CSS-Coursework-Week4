db6ce88195405dbb61a87a4c9874d39299512a6d
Ahmad Omid Omari <105078583+OmidOmar@users.noreply.github.com>
2022-07-29 21:09:22 +0100

    bugs fixed

--- Javascript/main.js
+++ Javascript/main.js
@@ -7,6 +7,27 @@ function myFunction(y) {
     x.style.display = "block";
   }
 }
+//Get the button
+var mybutton = document.getElementById("button-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
 function chkMediaQuery(x) {
   if (x.matches) {
     // If media query matches
@@ -19,3 +40,10 @@ function chkMediaQuery(x) {
 var x = window.matchMedia("(max-width: 768px)");
 chkMediaQuery(x); // Call listener function at run time
 x.addListener(chkMediaQuery);
+
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
