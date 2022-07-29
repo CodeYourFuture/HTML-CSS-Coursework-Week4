4b1402b3e3c2b9bd572e8e4db41a0c76d9ff4741
simeonbikov <bikovsem@gmail.com>
2022-07-29 21:10:26 +0100

    added scroll effect

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// document.div.classList.add(".need-animate");
+
+var velocity = 0.5;
+
+function update() {
+  var pos = $(window).scrollTop();
+  $(".container").each(function () {
+    var $element = $(this);
+    // subtract some from the height b/c of the padding
+    var height = $element.height() - 18;
+    $(this).css(
+      "backgroundPosition",
+      "50% " + Math.round((height - pos) * velocity) + "px"
+    );
+  });
+}
+
+$(window).bind("scroll", update);
