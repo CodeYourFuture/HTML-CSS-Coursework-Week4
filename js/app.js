914a9fd683f484f6fd408e1f98ed492086dc5712
Zaw Khing <42835438+ZawKhing@users.noreply.github.com>
2022-08-18 16:23:29 +0100

    fixing humburger menu

--- js/app.js
+++ js/app.js
@@ -1,7 +1,10 @@
-const toggleButton = document.getElementsByClassName('toggle-button')[0]
-const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+/*const toggleButton = document.getElementsByClassName('toggle-button')[0];
+const navbarLinks = document.getElementsByClassName('navbar-links')[0];
 
 toggleButton.addEventListener('click', () => {
     navbarLinks.classList.toggle('active')
-})
+})*/
+function myFunc(){
+    alert("You Clicked Me");
+ }
 
