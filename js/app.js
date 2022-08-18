bcd37e49b84d7a82150dec8b00ce2c2773107878
Zaw Khing <42835438+ZawKhing@users.noreply.github.com>
2022-08-18 22:59:42 +0100

    title alignment

--- js/app.js
+++ js/app.js
@@ -1,10 +1,19 @@
-/*const toggleButton = document.getElementsByClassName('toggle-button')[0];
-const navbarLinks = document.getElementsByClassName('navbar-links')[0];
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 
 toggleButton.addEventListener('click', () => {
     navbarLinks.classList.toggle('active')
-})*/
-function myFunc(){
-    alert("You Clicked Me");
- }
+})
 
+
+/*function myFunc(){
+    alert("You Clicked Me");
+ }*/
+
+ /*const hamburger = document.querySelector(".toggle-button");
+ const navMenu = document.querySelector(".navbar-links");
+
+ hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+ })*/
